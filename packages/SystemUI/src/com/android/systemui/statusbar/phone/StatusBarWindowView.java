omnirom/android_frameworks_base_old

[1/2] base: Double tap to sleep on lockscreen
Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -762,8 +762,12 @@ public class StatusBarWindowView extends FrameLayout implements Tunable {
         boolean doubleTapToSleepEnabled = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0,
                 UserHandle.USER_CURRENT) == 1;
+        boolean isDoubleTapEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN, 0,
+                UserHandle.USER_CURRENT) == 1;
         if (mNotificationPanel != null) {
             mNotificationPanel.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+            mNotificationPanel.setLockscreenDoubleTapToSleep(isDoubleTapEnabled);
         }
         if (mDragDownHelper != null) {
             mDragDownHelper.updateDoubleTapToSleep(doubleTapToSleepEnabled);

stag-tests/fwb

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -894,8 +894,12 @@ public class StatusBarWindowView extends FrameLayout {
         boolean doubleTapToSleepEnabled = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_DOUBLE_TAP_SLEEP_GESTURE, 0,
                 UserHandle.USER_CURRENT) == 1;
+        boolean isDoubleTapEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN, 0,
+                UserHandle.USER_CURRENT) == 1;
         if (mNotificationPanel != null) {
             mNotificationPanel.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+            mNotificationPanel.setLockscreenDoubleTapToSleep(isDoubleTapEnabled);
         }
         if (mDragDownHelper != null) {
             mDragDownHelper.updateDoubleTapToSleep(doubleTapToSleepEnabled);